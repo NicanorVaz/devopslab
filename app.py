6a04c42fe93edf3c55c86cdb939ddfaf527631b9
NicanorVaz <36211677+NicanorVaz@users.noreply.github.com>
2022-01-19 20:45:16 -0300

    Fix CSRF Erro build

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from flask_wtf.csrf import CSRFProtect
+from flask_wtf.csrf import CsrfProtect
 
 app = Flask(__name__)
 
-csrf = CSRFProtect(app)                                                                                                                           
+csrf = CsrfProtect(app)                                                                                                                           
 
 @app.route("/")
 def pagina_inicial():
